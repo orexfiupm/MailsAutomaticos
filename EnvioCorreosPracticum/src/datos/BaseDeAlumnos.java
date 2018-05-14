1dcd52afa45d662863ecc0bd7d828e38baccaaf6
lucianogg <lucgarc97@gmail.com>
2018-05-14 17:31:32 +0200

    Parse funciona aparentemente de forma perfecta

--- EnvioCorreosPracticum/src/datos/BaseDeAlumnos.java
+++ EnvioCorreosPracticum/src/datos/BaseDeAlumnos.java
@@ -14,10 +14,26 @@ public class BaseDeAlumnos {
     private LinkedList<TutorAcademico> bTutoresAcademicos;
     //private LinkedList<TutorProfesional> bTutoresProfesionales;
 
-    public BaseDeAlumnos() {
+    public LinkedList<Alumno> getbAlumnos() {
+		return bAlumnos;
+	}
+
+	public void setbAlumnos(LinkedList<Alumno> bAlumnos) {
+		this.bAlumnos = bAlumnos;
+	}
+
+	public LinkedList<TutorAcademico> getbTutoresAcademicos() {
+		return bTutoresAcademicos;
+	}
+
+	public void setbTutoresAcademicos(LinkedList<TutorAcademico> bTutoresAcademicos) {
+		this.bTutoresAcademicos = bTutoresAcademicos;
+	}
+
+	public BaseDeAlumnos() {
         bAlumnos = new LinkedList<>();
         bTutoresAcademicos = new LinkedList<>();
-        bTutoresProfesionales = new LinkedList<>();
+        //bTutoresProfesionales = new LinkedList<>();
     }
 
     public void addAlumno(Alumno alumno) {
@@ -25,6 +41,9 @@ public class BaseDeAlumnos {
     }
 
     public TutorAcademico addTutorAcademico(TutorAcademico tAcademico) {
+        if (tAcademico.esVacio()) {
+            return null;
+        }
         TutorAcademico tAcademicoYaExistente = null;
         for (TutorAcademico ta : bTutoresAcademicos) {
             if (ta.mismo(tAcademico)) {
@@ -61,24 +80,30 @@ public class BaseDeAlumnos {
 		Workbook w;
 		try {
             WorkbookSettings ws = new WorkbookSettings();
-            ws.setEncoding("CP1250");
+            // ws.setEncoding("CP1250");
+            ws.setEncoding("CP1252");
             w = Workbook.getWorkbook(new File(docPracticumPath), ws);
             Sheet sheet = w.getSheet(2); //Sheet numbers start by 1
 
-            for (int i=0; i<sheet.getRows(); i++) {
-                TutorAcademico tAcademico = addTutorAcademico(new TutorAcademico("", sheet.getCell(aOrden('U'), i).getContents(), sheet.getCell(aOrden('V'), i).getContents()));
-                addAlumno(new Alumno(sheet.getCell(aOrden('B'), i).getContents(), sheet.getCell(aOrden('A'), i).getContents(), sheet.getCell(aOrden('C'), i).getContents(), tAcademico));
+            for (int i=1; i<sheet.getRows(); i++) {
+                TutorAcademico tAcademico = addTutorAcademico(new TutorAcademico(sheet.getCell(aOrden('U'), i).getContents(), "", sheet.getCell(aOrden('V'), i).getContents()));
+                Alumno alumno = new Alumno(sheet.getCell(aOrden('B'), i).getContents(), sheet.getCell(aOrden('A'), i).getContents(), sheet.getCell(aOrden('C'), i).getContents(), tAcademico);
+                if (!alumno.esVacio()) {
+                    addAlumno(alumno);
+                    tAcademico.getTutorados().add(alumno);
+                }
+                
             }
 
-            for (Alumno alumno : bAlumnos) {
-                System.out.println(alumno.toString());
-            }
+            // for (Alumno alumno : bAlumnos) {
+            //     System.out.println(alumno.toString());
+            // }
 
-            System.out.println("Ahora tutores");
+            // System.out.println("Ahora tutores");
 
-            for (TutorAcademico tAcademico : bTutoresAcademicos) {
-                System.out.println(tAcademico.toString());
-            }
+            // for (TutorAcademico tAcademico : bTutoresAcademicos) {
+            //     System.out.println(tAcademico.toString());
+            // }
 
             //byte ptext[] = sheet.getCell(aOrden('U'), 0).getContents().getBytes("Cp1250"); 
             //String value = new String(ptext, "Cp1250"); 
---
+++ EnvioCorreosPracticum/src/parser/ParserConfiguracion.java
@@ -0,0 +1,158 @@
+package parser;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import datos.Alumno;
+import datos.TutorAcademico;
+import datos.TutorProfesional;
+
+public class ParserConfiguracion {
+    private String adjuntos;
+    private String tablasPath;
+    private String texto;
+    private String asunto;
+
+    public ParserConfiguracion(String path) throws FileNotFoundException {
+        BufferedReader bf = new BufferedReader(new FileReader(path));
+        try {
+            parseConfig(bf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getAdjuntos() {
+        return this.adjuntos;
+    }
+
+    public String getTablasSource() {
+        return this.tablasPath;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public String getAsunto() {
+        return this.asunto;
+    }
+
+    private void parseConfig(BufferedReader bf) throws IOException {
+        String line;
+        while ((line = bf.readLine()) != null) {
+            if (line.startsWith("RUTA_HOJA_PRACTICUM:")) {
+                // Tenemos en esta línea la tablaPath
+                String path = line.replace("RUTA_HOJA_PRACTICUM:", "").trim();
+                this.tablasPath = path;
+            } else if (line.startsWith("ADJUNTOS:")) {
+                String[] adjuntos = line.replace("ADJUNTOS:", "").trim().split(",");
+                for (String s : adjuntos) {
+                    s = "file://" + (s.trim());
+                }
+                String listaAdjuntos = "";
+                if (adjuntos.length > 0) {
+                    listaAdjuntos += adjuntos[0];
+                }
+                for (int i = 1; i < adjuntos.length; i++) {
+                    listaAdjuntos += ", " + adjuntos[i];
+                }
+                this.adjuntos = listaAdjuntos;
+            } else if (line.startsWith("TEXTO:")) {
+                String texto = line.replace("TEXTO:", "").trim() + "\n";
+                while ((line = bf.readLine()) != null) {
+                    texto += line + "\n";
+                }
+                this.texto = texto;
+            } else if (line.startsWith("ASUNTO:")) {
+                // Tenemos en esta línea la tablaPath
+                String path = line.replace("ASUNTO:", "").trim();
+                this.asunto = path;
+            }
+        }
+    }
+
+    public String getTextoPersonalizado(TutorAcademico tAcademico, TutorProfesional tProfesional, Alumno alumno) {
+        String text = this.texto;
+        if (alumno != null && !alumno.esVacio()) {
+            if (alumno.getApellidos() != null) {
+                text = text.replace("{{APELLIDOS}}", alumno.getApellidos());
+            }
+            if (alumno.getNombre() != null) {
+                text = text.replace("{{NOMBRE}}", alumno.getNombre());
+            }
+            if (alumno.getEmail() != null) {
+                text = text.replace("{{emailalumno}}", alumno.getEmail());
+            }
+            if (alumno.getNumeroMatricula() != null) {
+                text = text.replace("{{MATRICULA}}", alumno.getNumeroMatricula().toString());
+            }
+
+        }
+        if (tAcademico != null && !tAcademico.esVacio()) {
+            if (tAcademico.getNombre() != null) {
+                text = text.replace("{{TA}}", tAcademico.getNombre());
+            }
+            if (tAcademico.getEmail() != null) {
+                text = text.replace("{{emailalumno}}", tAcademico.getEmail());
+            }
+        }
+        if (tProfesional != null && !tProfesional.esVacio()) {
+            if (tProfesional.getEmpresa() != null) {
+                text = text.replace("{{EMPRESA}}", tProfesional.getEmpresa());
+            }
+            if (tProfesional.getNombre() != null) {
+                text = text.replace("{{TP}}", tProfesional.getNombre());
+            }
+            if (tProfesional.getEmail() != null) {
+                text = text.replace("{{emailTP}}", tProfesional.getEmail());
+            }
+            if (tProfesional.getTelefono() != null) {
+                text = text.replace("{{telTP}}", tProfesional.getTelefono());
+            }
+        }
+
+        return null;
+    }
+
+    public String getTextoPersonalizado(TutorAcademico tAcademico, TutorProfesional tProfesional, LinkedList<Alumno> alumnos) {
+        String text = this.texto;
+        System.out.println(text);
+        if (alumnos!=null) {
+            String[] splitSecuencia = texto.split("_SECUENCIADEALUMNOS_");
+            splitSecuencia[0] += "\n";
+            for (Alumno alumno : alumnos) {
+                splitSecuencia[0] += "\t" + alumno.toBeautifulStringSoloAlumno(false) + "\n";
+            }
+            text = splitSecuencia[0] + splitSecuencia[1];
+            System.out.println(text);
+        }
+        if (tAcademico != null && !tAcademico.esVacio()) {
+            if (tAcademico.getNombre() != null) {
+                text = text.replace("{{TA}}", tAcademico.getNombre());
+            }
+            if (tAcademico.getEmail() != null) {
+                text = text.replace("{{emailalumno}}", tAcademico.getEmail());
+            }
+        }
+        if (tProfesional != null && !tProfesional.esVacio()) {
+            if (tProfesional.getEmpresa() != null) {
+                text = text.replace("{{EMPRESA}}", tProfesional.getEmpresa());
+            }
+            if (tProfesional.getNombre() != null) {
+                text = text.replace("{{TP}}", tProfesional.getNombre());
+            }
+            if (tProfesional.getEmail() != null) {
+                text = text.replace("{{emailTP}}", tProfesional.getEmail());
+            }
+            if (tProfesional.getTelefono() != null) {
+                text = text.replace("{{telTP}}", tProfesional.getTelefono());
+            }
+        }
+
+        return text;
+    }
+}
