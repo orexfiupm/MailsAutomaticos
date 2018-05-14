a5fd09b24323e6fa6fb959fe16542171e84dcb22
lucianogg <lucgarc97@gmail.com>
2018-05-14 17:30:53 +0200

    Completa tutor academico

--- EnvioCorreosPracticum/src/datos/TutorAcademico.java
+++ EnvioCorreosPracticum/src/datos/TutorAcademico.java
@@ -1,9 +1,13 @@
 package datos;
 
+import java.util.LinkedList;
+
 public class TutorAcademico extends Persona {
+    private LinkedList<Alumno> tutorados;
 
     public TutorAcademico(String nombre, String apellidos, String email) {
         super(nombre, apellidos, email);
+        tutorados = new LinkedList<>();
     }
 
     public boolean mismo(TutorAcademico a) {
@@ -14,7 +18,19 @@ public class TutorAcademico extends Persona {
     }
 
     public String toString() {
-        return "tutorAcademico: {" + getNombre() + " " + getApellidos() + " " + getEmail() + "}";
+        String tutoradosString = "";
+        for (Alumno a : this.tutorados) {
+            tutoradosString += a.getNombre() + " " + a.getApellidos() + ", ";
+        }
+        return "tutorAcademico: {" + getNombre() + " " + getApellidos() + " " + getEmail() + " tutorados: " + tutoradosString + "}";
     }
 
+    public LinkedList<Alumno> getTutorados() {
+        return this.tutorados;
+    }
+
+    public boolean esVacio() {
+        return (getNombre()==null && getApellidos()==null && getEmail()==null) 
+            || (getNombre()=="" && getApellidos()=="" && getEmail()=="");
+    }
 }
