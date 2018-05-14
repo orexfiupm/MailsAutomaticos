654413fdfd32318195438a9433f4bce6952e3392
lucianogg <lucgarc97@gmail.com>
2018-05-14 17:31:54 +0200

    Parece ya ser capaz de enviar a TA

---
+++ EnvioCorreosPracticum/src/enviodecorreos/EnviaraTA.java
@@ -0,0 +1,59 @@
+package enviodecorreos;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import parser.*;
+import datos.*;
+
+public class EnviaraTA {
+	private static ParserConfiguracion pc;
+
+	public static void main(String[] args) {
+		String inputFile = args[1];
+		String inputFileConf = args[0];
+		BaseDeAlumnos bDeAlumnos = new BaseDeAlumnos();
+		
+		System.out.println("Working Directory = " +
+	              System.getProperty("user.dir"));
+
+		try {
+			bDeAlumnos.getAlumnosConDestinoFromFile(inputFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			pc = new ParserConfiguracion(inputFileConf);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Scanner sc = new Scanner(System.in);
+		for (TutorAcademico tAcademico : bDeAlumnos.getbTutoresAcademicos()) {
+			enviarCorreoAUnTutor(tAcademico, bDeAlumnos);
+			sc.nextLine();
+		}
+	}
+
+	private static void enviarCorreoAUnTutor(TutorAcademico tAcademico, BaseDeAlumnos bDeAlumnos) {
+		LinkedList<Alumno> alumnos = tAcademico.getTutorados();
+
+		String subject = pc.getAsunto();
+		String attachmentRoute = pc.getAdjuntos();
+		String bodyText = pc.getTextoPersonalizado(tAcademico, null, alumnos);
+
+		System.out.println("TA: " + tAcademico.getNombre() + "\nCorreo:\n" + bodyText);
+
+		try {
+			Runtime.getRuntime()
+					.exec(new String[] { "thunderbird", "-compose", "to='" + tAcademico.getEmail() + "'," + "subject='"
+							+ subject + "'," + "attachment='" + attachmentRoute + "'," + "body='" + bodyText + "'" });
+		} catch (IOException e1) {
+			System.out.println("IOException");
+			e1.printStackTrace();
+		}
+	}
+}
