f0da02ec2bcd8c1d9f079cdb2faad355420937c1
lucianogg <lucgarc97@gmail.com>
2018-05-14 17:30:19 +0200

    Alumno ahora tiene numero de matricula

--- EnvioCorreosPracticum/src/datos/Alumno.java
+++ EnvioCorreosPracticum/src/datos/Alumno.java
@@ -2,6 +2,7 @@ package datos;
 
 public class Alumno extends Persona {
     private TutorAcademico tAcademico;
+	private Integer numeroMatricula;
 
     public Alumno(String nombre, String apellidos, String email, TutorAcademico tAcademico) {
         super(nombre, apellidos, email);
@@ -23,4 +24,21 @@ public class Alumno extends Persona {
     public String toString() {
         return "alumno: {" + getNombre() + " " + getApellidos() + " " + getEmail() + " " + tAcademico.toString() + "}";
     }
+
+    public String toBeautifulStringSoloAlumno(boolean mostrarMatricula) {
+        return getApellidos() + ", " + getNombre() + ", email: " + getEmail() + ((getNumeroMatricula()!=null && mostrarMatricula)?(", número de matrícula: " + getNumeroMatricula()):(""));
+    }
+
+    public boolean esVacio() {
+        return (getNombre()==null && getApellidos()==null && getEmail()==null) 
+            || (getNombre()=="" && getApellidos()=="" && getEmail()=="");
+    }
+
+    public Integer getNumeroMatricula() {
+		return this.numeroMatricula;
+	}
+
+	public void setNumeroMatricula(Integer numeroMatricula) {
+		this.numeroMatricula = numeroMatricula;
+	}
 }
